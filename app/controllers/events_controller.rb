6af9bc0f38e73533f7d38ce1db3f67e9ea994887
Joey <gibbonsjoey95@gmail.com>
2023-10-04 12:05:11 -0500

    working on creating events

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,8 +7,25 @@ class EventsController < ApplicationController
     @event = Event.new 
   end
 
+  def create
+    @user = current_user
+    @event = @user.events.build(events_params)
+
+    if @event.save  
+      redirect_to @event
+    else
+      render :new
+    end
+  end
+
   def show
     @event = Event.find(params[:id])
     @creator = @event.creator
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:name, :date, :location)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :events, only: [:index, :new]
+  resources :events, only: [:index, :new, :create]
+  resources :users, only: [:show]
+
   devise_for :users
   root to: 'events#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
