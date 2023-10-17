1b7208447f19884b436c74a097793a828918a162
Joey <gibbonsjoey95@gmail.com>
2023-10-16 21:57:37 -0500

    more styling

--- app/javascript/left_sidebar.js
+++ app/javascript/left_sidebar.js
@@ -1,12 +1,16 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const toggleButton = document.getElementById('toggleEvents');
-    const userEvents = document.getElementById('userEvents');
-  
-    toggleButton.addEventListener('click', function() {
-      if (userEvents.style.display === 'none' || userEvents.style.display === '') {
-        userEvents.style.display = 'block';
-      } else {
-        userEvents.style.display = 'none';
-      }
+  const toggleButton = document.getElementById('toggleEvents');
+  const userEvents = document.getElementById('userEvents');
+
+  toggleButton.addEventListener('click', function() {
+    userEvents.style.display = userEvents.style.display === 'none' ? 'block' : 'none';
+    toggleButton.classList.toggle('active');
+  });
+
+  const eventLinks = document.querySelectorAll('.event-item a');
+  eventLinks.forEach(function(eventLink) {
+    eventLink.addEventListener('click', function(event) {
+      event.stopPropagation();
     });
   });
+});
