4e387c5ac4a7f303f56b86862fe0fc6a478bbac8
Joey <gibbonsjoey95@gmail.com>
2023-10-17 22:45:45 -0500

    working on the javascript

--- app/javascript/left_sidebar.js
+++ app/javascript/left_sidebar.js
@@ -1,16 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const toggleButton = document.getElementById('toggleEvents');
-  const userEvents = document.getElementById('userEvents');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const toggleButton = document.getElementById('toggleEvents');
+//   const userEvents = document.getElementById('userEvents');
 
-  toggleButton.addEventListener('click', function() {
-    userEvents.style.display = userEvents.style.display === 'none' ? 'block' : 'none';
-    toggleButton.classList.toggle('active');
-  });
+//   toggleButton.addEventListener('click', function() {
+//     userEvents.style.display = userEvents.style.display === 'none' ? 'block' : 'none';
+//     toggleButton.classList.toggle('active');
+//   });
+// });
 
-  const eventLinks = document.querySelectorAll('.event-item a');
-  eventLinks.forEach(function(eventLink) {
-    eventLink.addEventListener('click', function(event) {
-      event.stopPropagation();
-    });
-  });
+const toggleButton = document.getElementById('toggleEvents');
+const userEvents = document.getElementById('userEvents');
+
+toggleButton.addEventListener('click', function() {
+  userEvents.style.display = userEvents.style.display === 'none' ? 'block' : 'none';
+  toggleButton.classList.toggle('active');
 });
+
+
+
+
+
+
+
